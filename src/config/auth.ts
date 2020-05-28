1d4decaa0f3f342c79aecb114ef2c2ec4fb1e8b8
Thales <thalestestoni@gmail.com>
2020-05-28 20:44:48 -0300

    Middleware de autenticacao.

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '772ba54d84761b7e45200137dfbd616f',
+    expiresIn: '1d',
+  },
+};
---
+++ src/routes/sessions.routes.ts
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+
+import AthenticateUserService from '../services/AthenticateUserService';
+
+const sessionsRoutes = Router();
+
+sessionsRoutes.post('/', async (request, response) => {
+  try {
+    const { email, password } = request.body;
+
+    const athenticateUser = new AthenticateUserService();
+
+    const { user, token } = await athenticateUser.execute({ email, password });
+
+    delete user.password;
+
+    return response.json({ user, token });
+  } catch (error) {
+    return response.status(400).json({ error: error.message });
+  }
+});
+
+export default sessionsRoutes;
