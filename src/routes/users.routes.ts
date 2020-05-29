0d9705d714ea8a156a476f23fb590a624622d8f0
Thales <thalestestoni@gmail.com>
2020-05-28 21:12:42 -0300

    Upload de arquivos.

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
 
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
 const usersRoutes = Router();
+const upload = multer(uploadConfig);
 
 usersRoutes.post('/', async (request, response) => {
   try {
@@ -19,5 +24,14 @@ usersRoutes.post('/', async (request, response) => {
     return response.status(400).json({ error: error.message });
   }
 });
+
+usersRoutes.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    return response.json({ ok: true });
+  },
+);
 
 export default usersRoutes;
