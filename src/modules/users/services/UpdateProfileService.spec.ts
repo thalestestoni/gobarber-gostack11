15ddc79618adf614a080750a12db053f8962fd3f
Thales <thalestestoni@gmail.com>
2020-06-27 17:02:04 -0300

    Edicao de perfil

--- src/modules/users/services/UpdateProfileService.spec.ts
+++ src/modules/users/services/UpdateProfileService.spec.ts
@@ -36,6 +36,16 @@ describe('UpdateUserAvatar', () => {
     expect(updatedUser.email).toBe('johntre@example.com');
   });
 
+  it('should not be able update the profile from non-existing user', async () => {
+    expect(
+      updateUserProfile.execute({
+        user_id: 'non-existing-user-id',
+        name: 'Test',
+        email: 'test@email.com',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
   it('should not be able to change to another user email', async () => {
     await fakeUsersRepository.create({
       name: 'John Doe',
