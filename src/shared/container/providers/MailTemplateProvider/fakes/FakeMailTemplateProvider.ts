20e8a8f1290a09808972928794bcc9cc568db2b9
Thales <thalestestoni@gmail.com>
2020-06-27 15:36:28 -0300

    Template engine

--- src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
+++ src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
@@ -1,9 +1,8 @@
-import IParseMailTemplateDTO from '../dtos/IParseMailTemplateDTO';
 import IMailTemplateProviders from '../models/IMailTemplateProvider';
 
 class FakeMailTemplateProvider implements IMailTemplateProviders {
-  public async parse({ template }: IParseMailTemplateDTO): Promise<string> {
-    return template;
+  public async parse(): Promise<string> {
+    return 'Mail content';
   }
 }
 
