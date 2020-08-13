8bdfc838c023e78f20720a4e5346d37a10449f32
Thales <thalestestoni@gmail.com>
2020-08-13 11:49:49 -0300

    Emails pelo Amazon SES

---
+++ src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'thalestestoni@gmail.com',
+      name: 'Thales Testoni',
+    },
+  },
+} as IMailConfig;
