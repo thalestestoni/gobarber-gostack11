2df0b3b24fb0d3e81d0b17426b031efdf3406279
Thales <thalestestoni@gmail.com>
2020-08-14 12:35:35 -0300

    Express rate limit

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,11 +2,13 @@ import 'reflect-metadata';
 import 'dotenv/config';
 
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import { errors } from 'celebrate';
 import 'express-async-errors';
 
 import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
+import rateLimiter from './middlewares/rateLimiter';
 import routes from './routes';
 
 import '@shared/infra/typeorm';
@@ -14,6 +16,8 @@ import '@shared/container';
 
 const app = express();
 
+app.use(rateLimiter);
+app.use(cors);
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
