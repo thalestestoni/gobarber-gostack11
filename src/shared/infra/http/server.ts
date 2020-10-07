8097dfcafcfa11e924c3c06b599a7c787b6a82fe
Thales <thalestestoni@gmail.com>
2020-10-06 21:41:25 -0300

    Fix use cors

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -17,7 +17,7 @@ import '@shared/container';
 const app = express();
 
 app.use(rateLimiter);
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
